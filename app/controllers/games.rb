c53b986e4642fa60c3785356fc71c4e7ecc7bb14
Michael <email": null>
2016-02-04 14:18:55 -0800

    pre-heroku push

--- app/controllers/games.rb
+++ app/controllers/games.rb
@@ -1,41 +1,42 @@
 # Not all of these will be needed
+#Un-comment as needed
 
 
-# GET / View all games
-get '/games' do
-end
+# # GET / View all games
+# get '/games' do
+# end
 
-# GET / display form to create new Game
-get '/games/:new' do
+# # GET / display form to create new Game
+# get '/games/:new' do
 
-end
+# end
 
-# POST / create new Game from form
-post '/games' do
+# # POST / create new Game from form
+# post '/games' do
 
-# redirect to
+# # redirect to
 
-end
+# end
 
-# GET / View a single Game
-get '/games/:id' do
-end
+# # GET / View a single Game
+# get '/games/:id' do
+# end
 
-# GET / return form to update a Game
-# only allowed id logged in as creator of Game
-get '/games/:id/edit' do
+# # GET / return form to update a Game
+# # only allowed id logged in as creator of Game
+# get '/games/:id/edit' do
 
-end
+# end
 
-# PUT / use form results to update a Game
-put '/games/:id' do
+# # PUT / use form results to update a Game
+# put '/games/:id' do
 
-# redirect to that same Game
-end
+# # redirect to that same Game
+# end
 
-# DELETE a Game
-delete '/games/:id' do
+# # DELETE a Game
+# delete '/games/:id' do
 
-# redirect to index
+# # redirect to index
 
-end
+# end
